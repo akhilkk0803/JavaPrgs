a082e21fe7f0295c26126207f1ddecac457c27ff
akhilkk0803 <97751536+akhilkk0803@users.noreply.github.com>
2023-07-25 07:08:47 +0100

    Create Prg7.java

---
+++ Java/Prg7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class exception{
+
+public static void main(String []args){
+Scanner input=new Scanner(System.in);
+int a=input.nextInt();
+int b=input.nextInt();
+   try{
+ int  ans=a/b;
+ System.out.println(ans);
+ }
+   catch(Exception e ){
+     System.out.println("Divide by zero error");
+   }
+
+ }
+}
