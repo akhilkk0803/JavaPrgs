ac1e0d2cd214bd4aa6d3b79db5b46083404dd46c
akhilkk0803 <97751536+akhilkk0803@users.noreply.github.com>
2023-08-27 18:59:48 +0530

    Update Prg12.java

--- Java/Prg12.java
+++ Java/Prg12.java
@@ -6,10 +6,13 @@ public class Main
 		String s="akhil ak";
 		System.out.println(s.contains("a"));
 		int index=s.indexOf("a");
+		int count=0;
 		while(index!=-1){
 		    System.out.println(index);
+		     count++;
 		    index=s.indexOf("a",index+1);
 		}
+		System.out.println("Number of Occurences of a is "+count);
 	    
 	}
 }
