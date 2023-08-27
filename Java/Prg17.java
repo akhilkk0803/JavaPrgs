6e12886868c5afd37e03c439dcd1e1cf0a1b1ac0
akhilkk0803 <97751536+akhilkk0803@users.noreply.github.com>
2023-08-27 12:02:50 +0530

    Update Prg17.java

--- Java/Prg17.java
+++ Java/Prg17.java
@@ -1,7 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	String  date="04/25/1995";
+	Scanner input=new Scanner(System.in);
+	System.out.println("Enter Date in mm/dd/yyyy format");
+	String  date=input.nextLine();
 	String []months={" ","January","February","March","April","May","June","July","August","September",
 	"October","November","December"};
 	String temp=date.substring(0,2);
